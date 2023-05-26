a5421e304c2c58ba0ff1dedebefbeb1d612ea6cc
deswinta343 <127374291+deswinta343@users.noreply.github.com>
2023-05-26 16:45:23 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hello world, aku mau belajar programing")
+function dataDiri(name, nim) {
+    var student = {
+        name: name,
+        nim: nim
+    };
+
+    console.log(student);
+}
+
+// Contoh pemanggilan fungsi
+var name = "Deswinta Faadhilah Eka Susanto";
+var nim = "V3922014";
+
+dataDiri(name, nim);
+
+console.log(dataDiri.length)
